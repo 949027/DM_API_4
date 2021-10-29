0ca8b384a1a16dc2eeb684e315d914a6f4c5b717
949027 <949027@gmail.com>
2021-10-29 14:21:09 +0700

    Apply ifmain/def main

--- publisher.py
+++ publisher.py
@@ -6,16 +6,17 @@ from pathlib import Path
 import fetch_nasa
 import fetch_spacex
 
-if __name__ == '__main__':
+
+def main():
     Path('images').mkdir(parents=True, exist_ok=True)
     load_dotenv()
     token_nasa = os.getenv('TOKEN_NASA')
     token_bot = os.getenv('TOKEN_BOT_TELEGRAM')
     delay = float(os.getenv('DELAY', 86400))
 
-    #fetch_nasa.fetch_nasa_image('https://api.nasa.gov/planetary/apod', token_nasa)
-    #fetch_nasa.fetch_epic_image('https://api.nasa.gov/EPIC/api/natural/images', token_nasa)
-    #fetch_spacex.fetch_spacex_last_launch('https://api.spacexdata.com/v3/launches/74')
+    # fetch_nasa.fetch_nasa_image('https://api.nasa.gov/planetary/apod', token_nasa)
+    # fetch_nasa.fetch_epic_image('https://api.nasa.gov/EPIC/api/natural/images', token_nasa)
+    # fetch_spacex.fetch_spacex_last_launch('https://api.spacexdata.com/v3/launches/74')
 
     image_filenames = os.listdir('images')
 
@@ -27,3 +28,7 @@ if __name__ == '__main__':
                 bot.send_document(chat_id='@test_devman', document=file)
 
             time.sleep(delay)
+
+
+if __name__ == '__main__':
+    main()
