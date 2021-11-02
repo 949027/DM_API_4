45d0678187b157ad0f5f8b1c83e755625e04ba89
949027 <949027@gmail.com>
2021-11-02 17:01:02 +0700

    Remoted excess TABs

--- download.py
+++ download.py
@@ -5,7 +5,7 @@ def download_image(url, path, picture_number, picture_extension, payload=''):
     response = requests.get(url, params=payload)
     response.raise_for_status()
     with open(
-            '{}{}{}'.format(path, picture_number, picture_extension),
-            'wb',
+        '{}{}{}'.format(path, picture_number, picture_extension),
+        'wb',
     ) as file:
         file.write(response.content)
